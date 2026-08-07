2ad8b2f02b18359a15663e84c97b1fdb9aec13dc
tmlgde <tom.lagarde33@outlook.fr>
2026-08-07 13:10:28 +0200

    test push

--- linux_security/1x02_identity_management/4-pw_policy.sh
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-dpkg -s "$1" &>/dev/null || apt-get install -y "$1"
+dpkg -s "$1" &>/dev/null || apt-get install -y $1
 
-if grep -q "pam_quality.so" "$2"; then
+if grep -q "pam_pwquality.so" "$2"; then
 	sed -i 's/.*pam_pwquality\.so.*/password requisite pam_pwquality.so retry=3 minlen=12 minclass=3/' "$2"
 else
-	sed -i '/pam_unix.so/ i password requisite pam_pwquality.so retry=3 minless=12 miclass=3' "$2"
+	sed -i '/pam_unix.so/ i password requisite pam_pwquality.so retry=3 minlen=12 minclass=3' "$2"
 fi
