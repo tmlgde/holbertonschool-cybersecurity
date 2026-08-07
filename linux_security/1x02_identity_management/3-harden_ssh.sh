62a9823fb486e23d128958c600f30f260c7bfed8
tmlgde <tom.lagarde33@outlook.fr>
2026-08-07 11:49:46 +0200

    ajout flag tache 3

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -2,7 +2,3 @@
 sed -i -E 's/^#?PermitRootLogin.*/PermitRootLogin no/' "$1"
 sed -i -E 's/^#?PasswordAuthentication.*/PasswordAuthentication no/' "$1"
 sed -i -E 's/^#?PubkeyAuthentication.*/PubkeyAuthentication yes/' "$1"
-
-if sshd -t; then
-	systemctl reload ssh
-fi
