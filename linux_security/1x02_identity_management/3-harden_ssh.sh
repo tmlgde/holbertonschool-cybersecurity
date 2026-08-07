036c767a975688698e357cd9d2cc589b6c731e10
tmlgde <tom.lagarde33@outlook.fr>
2026-08-07 11:39:37 +0200

    test push

--- linux_security/1x02_identity_management/3-harden_ssh.sh
+++ linux_security/1x02_identity_management/3-harden_ssh.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 sed -i -E 's/^#?PermitRootLogin.*/PermitRootLogin no/' "$1"
 sed -i -E 's/^#?PasswordAuthentication.*/PasswordAuthentication no/' "$1"
 sed -i -E 's/^#?PubkeyAuthentication.*/PubkeyAuthentication yes/' "$1"
