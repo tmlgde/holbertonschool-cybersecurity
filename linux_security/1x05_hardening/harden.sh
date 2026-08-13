9b5f192678ca6e662246e860c8b4ffb383e77ae7
tmlgde <tom.lagarde33@outlook.fr>
2026-08-13 11:44:58 +0200

    ajout fichiers

---
+++ linux_security/1x05_hardening/harden.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ] then;
+	echo "This script must be run as root. Exiting."
+	exit 1
+fi
+
+log () {
+	local message=$1
+	echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> /var/log/hardening.log
+}
+
+source config/harden.cfg
+
+log "Hardening framework initialized"
+
+source lib/network.sh
+source lib/ssh.sh
+source lib/identity.sh
+source lib/system.sh
