3b90cff55a20b1323363d400600e0a0da069d369
tmlgde <tom.lagarde33@outlook.fr>
2026-08-19 11:29:52 +0200

    chercher avec addr la porte tun0

--- network_security/2x00_network_fundamentals/10-tunnel_ip.sh
+++ network_security/2x00_network_fundamentals/10-tunnel_ip.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-line=$(ip addr show tun0 | grep "inet ")
-echo "$line" | awk '{print $2}' | cut -d'/' -f1
+line=$(ip addr show tun0 | grep "inet "); echo "$line" | awk '{print $2}' | cut -d'/' -f1
