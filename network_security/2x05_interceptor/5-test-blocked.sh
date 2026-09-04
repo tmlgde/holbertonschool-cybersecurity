76e5acb89d4acd34d508d3d5d42198ad0e06bb46
tmlgde <tom.lagarde33@outlook.fr>
2026-09-04 13:36:25 +0200

    test avec les sites mis en place sur blocked_domains.txt

--- network_security/2x05_interceptor/5-test-blocked.sh
+++ network_security/2x05_interceptor/5-test-blocked.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://malware.com
+# Expect output: 403
+curl -x http://10.200.0.1:3128 -o /dev/null -s -w "%{http_code}" http://malware.com 
