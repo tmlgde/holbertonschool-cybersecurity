025c944e630f2ac334243f94648100503ea96b28
tmlgde <tom.lagarde33@outlook.fr>
2026-09-17 10:41:12 +0200

    lire nombre lignes d'un fichier

--- security_concepts/3x04_the_watchtower/0-stats.sh
+++ security_concepts/3x04_the_watchtower/0-stats.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "File: /var/log/auth.log - Lines: $(wc -l < /var/log/auth.log)"
-echo "File: /var/syslog/auth.log - Lines: $(wc -l < /var/log/syslog)"
+echo "File: /var/log/syslog - Lines: $(wc -l < /var/log/syslog)"
 echo "File: /var/kern/auth.log - Lines: $(wc -l < /var/log/kern.log)"
