8d90331cfe1c220301edfe03562dfbeae4e4ca84
tmlgde <tom.lagarde33@outlook.fr>
2026-09-17 11:34:39 +0200

    decommenter des lignes

--- security_concepts/3x04_the_watchtower/3-setup_loghost.sh
+++ security_concepts/3x04_the_watchtower/3-setup_loghost.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 sed -i 's/^#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
-sed -i 's/^#input(type="imudp" port="514"/input(type="imudp" port="514"/' /etc/rsyslog.conf
+sed -i 's/^#input(type="imudp" port="514"/input(type="imudp" port="514")/' /etc/rsyslog.conf
 sed -i 's/^#module(load="imtcp")/module(load="imtcp")/' /etc/rsyslog.conf
-sed -i 's/^#input(type="imtcp" port="514"/input(type="imtcp" port="514"/' /etc/rsyslog.conf
+sed -i 's/^#input(type="imtcp" port="514"/input(type="imtcp" port="514")/' /etc/rsyslog.conf
 service rsyslog restart
