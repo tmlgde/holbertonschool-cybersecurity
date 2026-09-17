a4a0ec36450731b9c9df5aa99a0e01baaf4401da
tmlgde <tom.lagarde33@outlook.fr>
2026-09-17 15:22:29 +0200

    rediriger logs ip et count enhtml

--- security_concepts/3x04_the_watchtower/9-html_report.sh
+++ security_concepts/3x04_the_watchtower/9-html_report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TRI=$(grep "Failed password" "$1" | awk '{print $(NF-3)}' | sort | uniq -c | sort -nr | head -5)
-cat << EOF > "$2"
+cat << EOF > $2
 <html><body>
 <h1>Security Report</h1>
 <table>
@@ -9,7 +9,7 @@ EOF
 echo "$TRI" | while read count ip; do
 	echo "<tr><td>$ip</td><td>$count</td></tr>" >> "$2"
 done
-cat << EOF >> "$2"
+cat << EOF >> $2
 </table>
 </body>
 </html>
