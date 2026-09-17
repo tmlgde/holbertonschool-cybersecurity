460c9bb55c58e85c74969e23ec1daf27d084589d
tmlgde <tom.lagarde33@outlook.fr>
2026-09-17 12:51:58 +0200

    ajoute la template json dans /etc/rsyslog.conf

--- security_concepts/3x04_the_watchtower/7-json_template.sh
+++ security_concepts/3x04_the_watchtower/7-json_template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 touch /etc/rsyslog.conf
-echo '$template json_fmt,"{"time":\"%timestamp%\", \"host\":\"%hostname%\", \"msg\":\"%msg%\"}"' >> /etc/rsyslog.conf
+echo '$template json_fmt,"{"time":\"%timestamp%\", "host":\"%hostname%\", "msg":\"%msg%\"}"' >> /etc/rsyslog.conf
