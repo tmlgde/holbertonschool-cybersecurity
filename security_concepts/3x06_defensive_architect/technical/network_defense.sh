d553bc9bfdcc57432c41811c0c278ecc45464e42
tmlgde <tom.lagarde33@outlook.fr>
2026-09-22 11:36:57 +0200

    creation des regles network avec UFW

--- security_concepts/3x06_defensive_architect/technical/network_defense.sh
+++ security_concepts/3x06_defensive_architect/technical/network_defense.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-WEB_SERVER_IP="10.0.1.10"
-BASTION_IP="10.0.0.5"
-
 configure_firewall() {
 	echo "Resetting UFW rules..."
 	ufw --force reset
@@ -14,10 +11,10 @@ configure_firewall() {
 	ufw default allow outgoing
 
 	echo "Allowing SSH from bastion host only..."
-	ufw allow from "$BASTION_IP" to any port 22
+	ufw allow from 10.0.0.5 to any port 22
 
 	echo " Allowing db from web server host only..."
-	ufw allow from "$WEB_SERVER_IP" to any port 5432
+	ufw allow from 10.0.1.10 to any port 5432
 
 	echo "Enabling UFW..."
 	ufw --force enable
