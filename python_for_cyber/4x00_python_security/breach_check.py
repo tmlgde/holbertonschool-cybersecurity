35a4da71cf3abcf0b0792d788c3811ce5306cd6e
tmlgde <tom.lagarde33@outlook.fr>
2026-09-22 15:26:20 +0200

    fonction readfile

--- python_for_cyber/4x00_python_security/breach_check.py
+++ python_for_cyber/4x00_python_security/breach_check.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 
 def main(): 
     """Parse les arguments et demarre l'outil"""
@@ -9,6 +10,20 @@ def main():
     parser.add_argument("-o","--output", help="path to the output report file")
     args = parser.parse_args()
     print("BreachCheck v1.0 startup...")
+
+def read_file(filename: str) -> list:
+    """Prend un fichier en entree et le lit comme une list"""
+    try:
+        with open(filename) as f:
+            data = f.readlines()
+        return data 
+    except FileNotFoundError:
+        print(f"[ERROR] File not found: {filename}", file=sys.stderr)
+        sys.exit(1)
+    except PermissionError:
+        print(f"[ERROR] Permission denied: {filename}", file=sys.stderr)
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
