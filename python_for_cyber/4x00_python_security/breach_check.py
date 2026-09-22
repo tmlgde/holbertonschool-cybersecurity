686d4176187b10fb3c043d6b21f436203f90a1dd
tmlgde <tom.lagarde33@outlook.fr>
2026-09-22 15:01:13 +0200

    parse des arguments

--- python_for_cyber/4x00_python_security/breach_check.py
+++ python_for_cyber/4x00_python_security/breach_check.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-def main(): #print
-    print("BreachCheck v1.0 startup...")
+import argparse
 
+def main(): 
+    """Parse les arguments et demarre l'outil"""
+    parser = argparse.ArgumentParser(description="BreachCheck!")
+    parser.add_argument("-f", "--file", required=True, type=str, help="Path to the input file to analyze")
+    parser.add_argument("-v","--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("-o","--output", help="path to the output report file")
+    args = parser.parse_args()
+    print("BreachCheck v1.0 startup...")
 
 if __name__ == "__main__":
     main()
