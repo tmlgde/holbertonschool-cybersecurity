1123383d8114499c3938101f339b9e152d0df02f
tmlgde <tom.lagarde33@outlook.fr>
2026-09-23 13:22:35 +0200

    deplacement validate_line

--- python_for_cyber/4x00_python_security/utils.py
+++ python_for_cyber/4x00_python_security/utils.py
@@ -14,3 +14,9 @@ def clean_data(lines: list) -> list:
             continue
         cleaned.append(stripped)
     return cleaned
+
+def validate_line(line: str) -> bool:
+    """utilisation d'une regex pour format mail:password"""
+    pattern = r"^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}):([^:]+)$"
+    match = re.fullmatch(pattern, line)
+    return match is not None
