fa80e52128681a0fd1c6b8a229ff5b61a6e34d34
tmlgde <tom.lagarde33@outlook.fr>
2026-09-23 13:23:41 +0200

    deplacement check_policy et hash_password

--- python_for_cyber/4x00_python_security/utils.py
+++ python_for_cyber/4x00_python_security/utils.py
@@ -20,3 +20,17 @@ def validate_line(line: str) -> bool:
     pattern = r"^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}):([^:]+)$"
     match = re.fullmatch(pattern, line)
     return match is not None
+
+def check_policy(password: str) -> str:
+    """Verification de sécurité pour le mot de passe"""
+    if len(password) < 8 or password.isalpha() or password in common_list:
+        return "WEAK"
+    else:
+        return "COMPLIANT"
+
+def hash_password(password: str, salt: str) -> str:
+    """transforme les mots de passe weak en hash"""
+    combined_bytes = password.encode() + salt.encode()
+    hash_obj = hashlib.sha256(combined_bytes)
+    hex_final = hash_obj.hexdigest()
+    return hex_final
